f5eac4a4631a278407c131bcbef6cedd242fc319
daniela.stamati <daniela.stamati@gmail.com>
2013-12-16 00:41:35 +0200

    tesseract sample call

--- Direct_Access_Image_Sample.cpp
+++ Direct_Access_Image_Sample.cpp
@@ -109,7 +109,7 @@ int executeBAMS(char dir[]){
 
 		strcpy(input,dir);
 		strcat(input,"\\\\");
-		strcat(input,"in4_grayscale.tif");
+		strcat(input,"in2_grayscale.tif");
 
 		strcat(cmd,input);
 		strcat(cmd, " ");
@@ -136,12 +136,36 @@ int executeBAMS(char dir[]){
 		cout<<"======================"<<retCode<<"\n\n";
 		
 	}
-
 	
-	
-	
-
 	return 0;
+}
+
+
+int tesseractOutput(){
+
+    char *outText;
+
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+
+    // Open input image with leptonica library
+    Pix *image = pixRead("C:\\Users\\Daniela\\Desktop\\VBAM\\BAMexe\\in2_grayscale.tif");
+    api->SetImage(image);
+    // Get OCR result
+    outText = api->GetUTF8Text();
+    printf("OCR output:\n%s", outText);
+
+    // Destroy used object and release memory
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
+
+    return 0;
+
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -161,6 +185,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	WideCharToMultiByte(CP_ACP,0,argv[1],-1, dir,260,&DefChar, NULL);	
 	getExeFiles(argv);
 	executeBAMS(dir);
+	tesseractOutput();
 	
     //Return with success
     return 0;
