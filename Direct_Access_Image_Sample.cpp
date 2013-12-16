0e33ba9696240d1281a60d17b62abb3fc46dd6fd
daniela.stamati <daniela.stamati@gmail.com>
2013-12-16 14:13:00 +0200

    Running BAMs on every image in the BAMexe folder

--- Direct_Access_Image_Sample.cpp
+++ Direct_Access_Image_Sample.cpp
@@ -29,7 +29,66 @@ using namespace std;
 //===========================================================================
 
 vector<char*> exeList;
+vector<char*> tifList;
 TCHAR BAMdirectoryPath[MAX_PATH];
+
+int getTifFiles(_TCHAR* argv[]){
+
+	TCHAR path[MAX_PATH];
+	DWORD dwError=0;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	WIN32_FIND_DATA ffd;
+	LARGE_INTEGER filesize;
+	string tif_extension = ".tif";	
+	tifList.clear();
+	
+	// Prepare string for use with FindFile functions.  First, copy the
+	// string to a buffer, then append '\*' to the directory name.
+	StringCchCopy(path, MAX_PATH, argv[1]);
+	StringCchCat(path, MAX_PATH, TEXT("\\*"));
+	hFind = FindFirstFile(path, &ffd);
+
+	if (INVALID_HANDLE_VALUE == hFind) 
+	{
+		//TODO handle error
+		return dwError;
+	} 
+   
+	
+
+	do
+	{
+		if (!(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			//convert from wide char to narrow char array WCHAR=>char=>string
+			char ch[260];
+			char DefChar = ' ';
+			WideCharToMultiByte(CP_ACP,0,ffd.cFileName,-1, ch,260,&DefChar, NULL);			
+			string ss(ch);
+
+			if(ss.rfind(tif_extension)==ss.length()-4){
+				tifList.push_back(strdup(ch));			
+			}else{
+				continue;
+			}
+			
+			cout<<"|||"<<ch<<"||||\n";
+			cout<<ss.length()<<"\n";
+		}
+	
+	}while (FindNextFile(hFind, &ffd) != 0);
+ 
+	dwError = GetLastError();
+	if (dwError != ERROR_NO_MORE_FILES) 
+	{
+		cout<<"Received error: "<<dwError;
+		return dwError;
+	}
+
+	FindClose(hFind);
+	return 0;
+
+}
 
 int getExeFiles(_TCHAR* argv[]){
 
@@ -90,7 +149,7 @@ int getExeFiles(_TCHAR* argv[]){
 
 int executeBAMS(char dir[]){
 
-	int i;
+	int i,j;
 
 	char exe[MAX_PATH];
 	char input[MAX_PATH];
@@ -99,41 +158,48 @@ int executeBAMS(char dir[]){
 	char cmd[500];
 	char filename[100];
 
+	cout<<"~~~~~~~~~exeList~~~~~~~~~~~~"<<exeList.size()<<"\n\n";
+	cout<<"~~~~~~~~~tifList~~~~~~~~~~~~"<<tifList.size()<<"\n\n";
+
+
 	for(i = 0; i<exeList.size();i++){
-		strcpy(exe,dir);
-		strcat(exe,"\\\\");
-		strcat(exe,exeList.at(i));
 
-		strcpy(cmd,exe);
-		strcat(cmd," ");
+		for (j = 0; j<tifList.size(); j++){
+			strcpy(exe,dir);
+			strcat(exe,"\\\\");
+			strcat(exe,exeList.at(i));
 
-		strcpy(input,dir);
-		strcat(input,"\\\\");
-		strcat(input,"in2_grayscale.tif");
+			strcpy(cmd,exe);
+			strcat(cmd," ");
 
-		strcat(cmd,input);
-		strcat(cmd, " ");
+			strcpy(input,dir);
+			strcat(input,"\\\\");
+			strcat(input,tifList.at(j));
 
-		strcpy(output,dir);
-		strcat(output,"\\\\");
-		sprintf(filename,"out%d.tif",i+1);
-		strcat(output,filename);
+			strcat(cmd,input);
+			strcat(cmd, " ");
 
-		strcat(cmd,output);
-		strcat(cmd, " ");
+			strcpy(output,dir);
+			strcat(output,"\\\\");
+			sprintf(filename,"out%d.tif",j+1);
+			strcat(output,filename);
 
-		strcpy(output_conf,dir);
-		strcat(output_conf,"\\\\");
-		sprintf(filename,"out_conf%d.tif",i+1);
-		strcat(output_conf,filename);
+			strcat(cmd,output);
+			strcat(cmd, " ");
 
-		strcat(cmd,output_conf);
+			strcpy(output_conf,dir);
+			strcat(output_conf,"\\\\");
+			sprintf(filename,"out_conf%d.tif",j+1);
+			strcat(output_conf,filename);
 
-		cout<<"output: "<<output<<"\n\n";
-		cout<<"CMD: "<<cmd<<"\n\n";
+			strcat(cmd,output_conf);
+		
+			//cout<<"output: "<<output<<"\n\n";
+			cout<<"CMD: "<<cmd<<"\n\n";
 
-		int retCode = system(cmd);
-		cout<<"======================"<<retCode<<"\n\n";
+			int retCode = system(cmd);
+			//cout<<"======================"<<retCode<<"\n\n";
+		}
 		
 	}
 	
@@ -184,8 +250,11 @@ int _tmain(int argc, _TCHAR* argv[])
     }
 	WideCharToMultiByte(CP_ACP,0,argv[1],-1, dir,260,&DefChar, NULL);	
 	getExeFiles(argv);
+	getTifFiles(argv);
 	executeBAMS(dir);
-	tesseractOutput();
+	//tesseractOutput();
+
+	//TODO: delete output files
 	
     //Return with success
     return 0;
